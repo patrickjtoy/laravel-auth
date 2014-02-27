be3a642bef4fff8b6b47ee6d99af6df92622b11e
Patrick Toy <patrick@regulusdesigns.com>
2014-02-26 20:52:03 -0500

    Modified users migration file

--- app/database/migrations/2014_02_27_013102_create-users-table.php
+++ app/database/migrations/2014_02_27_013102_create-users-table.php
@@ -12,7 +12,12 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		//
+		$table->increments('id');
+		$table->string('firstname', 20);
+		$table->string('lastname', 20);
+		$table->string('email', 100)->unique();
+		$table->string('password', 64);
+		$table->timestamps();
 	}
 
 	/**
@@ -22,7 +27,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		//
+		Schema::drop('users');
 	}
 
 }
