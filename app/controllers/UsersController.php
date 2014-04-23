d8490c12222b2fdcfbdcd7c3f9cdee15a570d356
Patrick Toy <patrick@regulusdesigns.com>
2014-04-23 12:57:06 -0400

    Addition
    1. Created login view
    2. Added validation rules

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -3,7 +3,34 @@
 class UsersController extends BaseController {
 	protected $layout = "layouts.main";
 
+	public function __construct() {
+		$this->beforeFilter('csrf', array('on' => 'post'));
+	}
+
 	public function getRegister() {
 		$this->layout->content = View::make('users.register');
+	}
+
+	public function postCreate() {
+		$validator = Validator::make(Input::all(), User::$rules);
+
+		if($validator->passes()) {
+			// validation has passed, save user in DB
+			$user = new User;
+			$user->firstname = Input::get('firstname');
+			$user->lastname = Input::get('lastname');
+			$user->email = Input::get('email');
+			$user->password = Hash::make(Input::get('password'));
+			$user->save();
+
+			return Redirect::to('users/login')->with('message', 'Thanks for registering!');
+		} else {
+			// validation has failed, display error messages
+			return Redirect::to('users/register')->with('message', 'The following errors occurred')->withErrors($validator)->withInput();
+		}
+	}
+
+	public function getLogin() {
+		$this->layout->content = View::make('users.login');
 	}
 }
---
+++ app/views/users/login.blade.php
@@ -0,0 +1,12 @@
+{{ Form::open(array('url' => 'users/signin', 'class' => 'form-signin')) }}
+	<h2 class="form-signin-heading">Please Login</h2>
+
+	<div class="form-group">
+		{{ Form::text('email', null, array('class' => 'form-control', 'placeholder' => 'Email Address')) }}
+	</div>
+	<div class="form-group">
+		{{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Password')) }}
+	</div>
+
+	{{ Form::submit('Login', array('class' => 'tn btn-large btn-primary btn-block')) }}
+{{ Form::close() }}
